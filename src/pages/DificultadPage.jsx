dffe34249b77037f4358a514fbcec9087faa0c39
Nicole-Soares <nicolesoares918@gmail.com>
2025-12-02 12:04:38 -0300

    se hicieron mejoras en css

--- src/pages/DificultadPage.jsx
+++ src/pages/DificultadPage.jsx
@@ -1,57 +1,125 @@
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Loader from "../components/Loader";
 import "../style/DificultadPage.css";
-import personajes from "../assets/personajesPreguntados.png"
+import Arte from "../assets/Arte.png";
+import Ciencia from "../assets/Ciencia.png";
+import Deportes from "../assets/Deportes.png";
+import Historia from "../assets/Historia.png";
+import Willy from "../assets/Willy.png";
+import Entrenemiento from "../assets/Entretenimiento.png";
+import Geografia from "../assets/Geografia.png";
 
-export default function DificultadPage(){
-    const [dificultades, setDificultades] = useState([]);
-    const [dificultadSeleccionada, setDifilcultadSeleccionada] = useState(null);
+export default function DificultadPage() {
+  const [dificultades, setDificultades] = useState([]);
+  const [dificultadSeleccionada, setDifilcultadSeleccionada] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://preguntados-api.vercel.app/api/difficulty');
-                const result = await response.json();
-                setDificultades(result);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const MIN_LOADING_MS = 2050; // mínimo que querés ver el loader
+    const start = Date.now(); // el tiempo en el que empezo a montarse el componente
 
-    const handleClickDificultad = (dificultad) => {
-        setDifilcultadSeleccionada(dificultad);
-    }
+    
 
-    return(
-        <div className="dificultad-background">
-            <img src={personajes} className="personajes-top" />
-           <div className="modal-dificultad">
-                <h1 className="modal-title">Seleccioná la dificultad</h1>
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "https://preguntados-api.vercel.app/api/difficulty"
+        );
+        const result = await response.json();
 
-                {dificultades.length > 0 ? (
-                    dificultades.map(dificultad => (
-                        <button 
-                            key={dificultad} 
-                            className={`dificultad-btn ${dificultadSeleccionada === dificultad ? "seleccionado" : ""}`}
-                            onClick={() => handleClickDificultad(dificultad)}
-                        >
-                            {dificultad}
-                        </button>
-                    ))
-                ) : (
-                    <Loader/>
-                )}
 
-                <button 
-                    className="jugar-btn" 
-                    disabled={!dificultadSeleccionada}
-                >
-                    ¡A jugar!
-                </button>
-            </div>
+        setDificultades(result);
 
-        </div>
-    )
+        // cuánto tardó realmente
+        const elapsed = Date.now() - start;
+        const remaining = Math.max(MIN_LOADING_MS - elapsed, 0);
+
+        // terminamos el loading después de completar el mínimo
+        setTimeout(() => {
+          setLoading(false);
+        }, remaining);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleClickDificultad = (dificultad) => {
+    setDifilcultadSeleccionada(dificultad);
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="dificultad-background">
+      <div className="contenedor-personajes">
+        <img
+          src={Ciencia}
+          className="personajes-top personajes-animados"
+          alt="ciencia"
+        />
+        <img
+          src={Deportes}
+          className="personajes-top personajes-animados"
+          alt="deportes"
+        />
+        <img
+          src={Historia}
+          className="personajes-top personajes-animados"
+          alt="historia"
+        />
+        <img
+          src={Willy}
+          className="personajes-top personajes-animados personaje-willy"
+          alt="personajes"
+        />
+        <img
+          src={Entrenemiento}
+          className="personajes-top personajes-animados"
+          alt="entetenimiento"
+        />
+        <img
+          src={Geografia}
+          className="personajes-top personajes-animados"
+          alt="geografia"
+        />
+        <img
+          src={Arte}
+          className="personajes-top personajes-animados"
+          alt="arte"
+        />
+      </div>
+      <div className="modal-dificultad">
+        <h1 className="modal-title">Seleccioná la dificultad</h1>
+
+        {dificultades.length < 0 ? (
+          <div>
+            <h1>No hay dificultades disponibles</h1>
+          </div>
+        ) : (
+          dificultades.map((dificultad) => (
+            <button
+              key={dificultad}
+              className={`dificultad-btn ${
+                dificultadSeleccionada === dificultad ? "seleccionado" : ""
+              }`}
+              onClick={() => handleClickDificultad(dificultad)}
+            >
+              {dificultad}
+            </button>
+          ))
+        )}
+
+        <button className="jugar-btn" disabled={!dificultadSeleccionada}>
+          ¡A jugar!
+        </button>
+      </div>
+    </div>
+  );
 }
